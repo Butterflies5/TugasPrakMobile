c9d4d9582e1434bdbb44950a366592ca1417dacb
Richeko Kamila Balqisya <113596339+Butterflies5@users.noreply.github.com>
2022-10-02 18:06:26 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,86 +1,321 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "List tourism",
+    home: MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Login Screen';
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("List Destination")),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15),
+            child: Text("Gambar Destinasi Wisata"),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/12/6b/63/0b/bosscha-observatory.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/0d/c2/e7/e6/quotes-kota-bandung.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/15/01/d7/4b/p-20180510-153310-01.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/0d/c2/e7/e6/quotes-kota-bandung.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/08/8b/87/50/bandung-movie-park.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-w/19/1c/8e/f7/geology-museum.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/17/f9/ff/f8/floating-market-bandung.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/0b/6e/7c/ce/rocks-sticking-out-of.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://media-cdn.tripadvisor.com/media/photo-o/1a/e0/7f/9c/kampung-cai-ranca-upas.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15, left: 15),
+            child: Text("List All Destination"),
+          ),
+
+
+          ListView(
+            shrinkWrap: true,
+
+            physics: NeverScrollableScrollPhysics(),
+
+            children: [
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Farm House Lembang"),
+                  subtitle: Text(
+                      "openDays: Open Everyday, openTime: 09:00 - 20:00, ticketPrice: Rp 25000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-o/11/3f/04/39/p-20171111-110220-largejpg.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Observatorium Bosscha"),
+                  subtitle: Text(
+                      "openDays: Open Weekend, openTime: 09:00 - 14:30, ticketPrice: Rp 20000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-s/0a/ef/36/e2/jalan-asia-afrika.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Jalan Asia Afrika"),
+                  subtitle: Text(
+                      "openDays: Open Everydays, openTime: 24 hours, ticketPrice: free"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-o/0d/a2/cb/05/stone-garden-citatah.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Stone Garden"),
+                  subtitle: Text(
+                      "openDays: Open Everyday, openTime: 06:00 - 17:00, ticketPrice: Rp 3000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-w/09/73/33/05/taman-film-pasopati.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Taman Film Pasopati"),
+                  subtitle: Text(
+                      "openDays: Open Weekdays, openTime: 24 hours, ticketPrice: Rp free"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-s/1a/55/e0/dc/geology-museum.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Museum Geologi"),
+                  subtitle: Text(
+                      "openDays: Open Weekdays, openTime: 09:00 - 15:30, ticketPrice: Rp 3000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-p/19/ce/b4/9b/img20181224120857-largejpg.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Floating Market"),
+                  subtitle: Text(
+                      "openDays: Open Everyday, openTime: 09:00 - 17:00, ticketPrice: Rp 20000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-o/0a/8b/9a/79/2945-t00572-www-initempatwisat.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Kawah Putih"),
+                  subtitle: Text(
+                      "openDays: Open Everday, openTime: 07:00 - 17:00, ticketPrice: Rp 35000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+              ListTile(
+                  leading: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        image: DecorationImage(
+                            image: NetworkImage("https://media-cdn.tripadvisor.com/media/photo-w/13/ee/27/0a/ranca-upas.jpg"),
+                            fit: BoxFit.cover)),
+                  ),
+                  title: Text("Ranca Upas"),
+                  subtitle: Text(
+                      "openDays: Open Everydays, openTime: 24 hours, ticketPrice: Rp 20000"),
+                  trailing: Icon(Icons.bookmark)
+              ),
+            ],
+          ),
+        ],
       ),
     );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            SizedBox(height: 50,
-              width: 55,
-            ),
-            Container(
-                height: 35,
-                padding: const EdgeInsets.symmetric(horizontal: 55),
-                child: ElevatedButton(
-                  child: const Text('Login In'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-          ],
-        ));
   }
 }
